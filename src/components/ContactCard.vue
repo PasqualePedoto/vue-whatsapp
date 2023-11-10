d9bc5c3779b03d8ab1e035cd61a8051bd238cd1b
PasqualePedoto <pasquale.pedoto123@gmail.com>
2023-11-10 17:57:44 +0100

    Modify message confermation into the chat and contact card

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -10,7 +10,23 @@
           {{ lastMessage }}
         </div>
         <div class="visualization">
-          <i class="fa-solid fa-check-double" style="color: #3cc809"></i>
+          <span class="metadata">
+            <span class="tick"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="15"
+                id="msg-dblcheck-ack"
+                x="2063"
+                y="2076"
+              >
+                <path
+                  d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z"
+                  fill="#4fc3f7"
+                />
+              </svg>
+            </span>
+          </span>
         </div>
       </div>
     </div>
@@ -85,5 +101,50 @@ img {
 }
 
 .visualization {
+  .metadata {
+    display: inline-block;
+    float: right;
+    padding: 0 0 0 7px;
+    position: relative;
+    bottom: -4px;
+  }
+
+  .metadata .tick {
+    display: inline-block;
+    margin-left: 2px;
+    position: relative;
+    top: 4px;
+    height: 16px;
+    width: 16px;
+  }
+
+  .metadata .tick svg {
+    position: absolute;
+    transition: 0.5s ease-in-out;
+  }
+
+  .metadata .tick svg:first-child {
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    -webkit-transform: perspective(800px) rotateY(180deg);
+    transform: perspective(800px) rotateY(180deg);
+  }
+
+  .metadata .tick svg:last-child {
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    -webkit-transform: perspective(800px) rotateY(0deg);
+    transform: perspective(800px) rotateY(0deg);
+  }
+
+  .metadata .tick-animation svg:first-child {
+    -webkit-transform: perspective(800px) rotateY(0);
+    transform: perspective(800px) rotateY(0);
+  }
+
+  .metadata .tick-animation svg:last-child {
+    -webkit-transform: perspective(800px) rotateY(-179.9deg);
+    transform: perspective(800px) rotateY(-179.9deg);
+  }
 }
 </style>
